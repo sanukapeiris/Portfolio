8b39752bb81e3131946e28a56576be37134e666a
sanukapeiris <sanukapeiris6000@gmail.com>
2024-04-16 11:08:01 +0530

    Readme

--- Portfolio/js/main.js
+++ Portfolio/js/main.js
@@ -51,7 +51,19 @@ const sr = ScrollReveal({
 //     reset: true
 });
 
-sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text' ,{});
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
-sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200});
+
+
+<!-- Skill box -->
+src="https://unpkg.com/scrollreveal"
+    // Initialize Scroll Reveal
+    ScrollReveal().reveal('.skill-box1', {
+    delay: 300,
+    distance: '50px',
+    origin: 'bottom',
+    easing: 'ease-in-out',
+    interval: 200
+});
